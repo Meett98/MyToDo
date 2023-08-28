c461912fe71e3a250475885c2a81cb7ec0ff8e84
Meet <prajapatimeet908@gmail.com>
2023-08-28 18:13:40 +0530

    Updated the Broadcast receiver class

--- app/src/main/java/com/example/mytodos/NetworkChangeReceiver.kt
+++ app/src/main/java/com/example/mytodos/NetworkChangeReceiver.kt
@@ -12,10 +12,8 @@ class NetworkChangeReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == ConnectivityManager.CONNECTIVITY_ACTION) {
             val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork: Network? = cm.activeNetwork
-            val networkCapabilities: NetworkCapabilities? = cm.getNetworkCapabilities(activeNetwork)
-
-            val isConnected = networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+            val activeNetwork = cm.activeNetworkInfo
+            val isConnected = activeNetwork?.isConnectedOrConnecting == true
 
             val status = if (isConnected) "Connected" else "Disconnected"
             Toast.makeText(context, "Network Status: $status", Toast.LENGTH_SHORT).show()
